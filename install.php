c508078fb353c8fd33f7a93bc4bfa12b86430245
Emil Kilhage <emilkilhage@gmail.com>
2012-04-25 08:36:27 +0200

    Made some changes to the install script

--- install.php
+++ install.php
@@ -1,27 +1,38 @@
 <?php
 
+/**
+ * Should be executed from cli
+ * @author Emil Kilhage
+ */
+
+$help = "
+****************************************************
+Installs an empty yaml vardef addition for a module
+****************************************************
+\$ php install.php [module] [module] [module] [....]
+
+";
+
+php_sapi_name() === 'cli' or die("Could only be executed from cli!");
+isset($argv[1]) or die($help);
+
 $dir = __DIR__;
 
 while (1) {
     if (file_exists($dir . '/sugar_version.php')) break;
     $dir = dirname($dir);
-    if ($dir == '/') die("Unable to find SugarCrm root.");
+    $dir !== '/' or die("Unable to find SugarCrm root.");
 }
 
 chdir($dir);
 define("sugarEntry", true);
 require_once 'include/entryPoint.php';
 
-$help = "
-\$ php install.php [module] [module] [module] [....]
-
-";
-
 $modifier_dir = str_replace("$dir/", '', __DIR__);
 
-isset($argv[1]) or die($help);
-
 global $beanList;
+
+$date = date('Y-m-d');
 
 foreach (array_slice($argv, 1) as $module)
 {
@@ -39,6 +50,10 @@ foreach (array_slice($argv, 1) as $module)
     $php_file = <<<PHP
 <?php
 
+/*********************************************
+ * Vardef addition for module: $module       *
+ * Created: $date                            *
+ ********************************************/
 require_once '$modifier_dir/VardefModifier.php';
 \$dictionary = VardefModifier::modify("$module", \$dictionary)->
     yaml("$yml_path")->
@@ -46,13 +61,22 @@ require_once '$modifier_dir/VardefModifier.php';
 
 
 PHP;
-    $yml_file = "";
+    $yml_file = <<<YAML
+################################################
+# Vardef addition for module: $module          #
+# Created: $date                               #
+################################################
+
+
+YAML;
+
     echo "Creating: $php_path \n";
     is_dir($php_dir) or mkdir($php_dir. 0755, true);
     file_put_contents($php_path, $php_file);
+
     echo "Creating: $yml_path \n";
     is_dir($yml_dir) or mkdir($yml_dir. 0755, true);
     file_put_contents($yml_path, $yml_file);
 }
 
-echo "\nDone!\n";
+echo "\n* Done!\n";
