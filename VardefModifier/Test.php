0465ee8b22c4568b485f7a9fdea74c417bf44a03
Emil Kilhage <emilkilhage@gmail.com>
2012-05-09 22:53:26 +0200

    Added missing tests and improved the documentation

--- VardefModifier/Test.php
+++ VardefModifier/Test.php
@@ -27,6 +27,24 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
         if (!isset($beanList['Cases']))
             $beanList['Cases'] = 'aCase';
 
+        if (!isset($beanList['Tasks']))
+            $beanList['Tasks'] = 'Task';
+
+        if (!isset($beanList['ProductTypes']))
+            $beanList['ProductTypes'] = 'ProductType';
+
+        if (!isset($beanList['Notes']))
+            $beanList['Notes'] = 'Note';
+
+        if (!isset($beanList['Meetings']))
+            $beanList['Meetings'] = 'Meeting';
+
+        if (!isset($beanList['Calls']))
+            $beanList['Calls'] = 'Call';
+
+        if (!isset($beanList['Emails']))
+            $beanList['Emails'] = 'Email';
+
         $this->module_name = "_MyModules";
         $this->object_name = "_MyModule";
         $beanList[$this->module_name] = $this->object_name;
@@ -45,10 +63,14 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
         unset($dictionary[$this->object_name]);
     }
 
+    /**
+     * @global type $dictionary
+     * @return \VardefModifier
+     */
     private function create()
     {
         global $dictionary;
-        return new VardefModifier($this->module_name, $dictionary);
+        return VardefModifier::modify($this->module_name, $dictionary);
     }
 
     public function test_Varchar()
@@ -193,6 +215,8 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'importable' => 'true',
                     'massupdate' => false,
                     'type' => 'date',
+                    'enable_range_search' => true,
+                    'options' => 'date_range_search_dom',
                 ),
             ),
             'indices' => array (),
@@ -301,7 +325,9 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'importable' => 'true',
                     'massupdate' => false,
                     'type' => 'datetimecombo',
-                    'dbType' => 'datetime'
+                    'dbType' => 'datetime',
+                    'enable_range_search' => true,
+                    'options' => 'date_range_search_dom',
                 ),
             ),
             'indices' => array (),
@@ -321,6 +347,239 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
         ));
         $dic = $m->get();
         $this->assertEquals($real_dic, $dic[$this->object_name]);
+    }
+
+    public function test_Address()
+    {
+        $rd = array (
+            '_MyModule' => array (
+                'favorites' => true,
+                'fields' => array (
+                    'primary_address_street' => array (
+                        'name' => 'primary_address_street',
+                        'vname' => 'LBL_PRIMARY_ADDRESS_STREET',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 150,
+                        'merge_filter' => 'enabled',
+                        'group' => 'primary_address',
+                    ),
+                    'primary_address_city' => array (
+                        'name' => 'primary_address_city',
+                        'vname' => 'LBL_PRIMARY_ADDRESS_CITY',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 100,
+                        'merge_filter' => 'enabled',
+                        'group' => 'primary_address',
+                    ),
+                    'primary_address_state' => array (
+                        'name' => 'primary_address_state',
+                        'vname' => 'LBL_PRIMARY_ADDRESS_STATE',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 100,
+                        'merge_filter' => 'enabled',
+                        'group' => 'primary_address',
+                    ),
+                    'primary_address_postalcode' => array (
+                        'name' => 'primary_address_postalcode',
+                        'vname' => 'LBL_PRIMARY_ADDRESS_POSTALCODE',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 20,
+                        'merge_filter' => 'enabled',
+                        'group' => 'primary_address',
+                    ),
+                    'primary_address_country' => array (
+                        'name' => 'primary_address_country',
+                        'vname' => 'LBL_PRIMARY_ADDRESS_COUNTRY',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 255,
+                        'merge_filter' => 'enabled',
+                        'group' => 'primary_address',
+                    ),
+                ),
+                'indices' => array (),
+                'relationships' => array (),
+            ),
+        );
+        $dic = $this->create()
+            ->addField('primary', 'address')
+            ->get();
+        $this->assertEquals($rd, $dic);
+    }
+
+    public function test_setDefault()
+    {
+        $rd = array (
+            '_MyModule' => array (
+                'favorites' => true,
+                'fields' => array (
+                    'my' => array (
+                        'name' => 'my',
+                        'vname' => 'LBL_MY',
+                        'required' => false,
+                        'reportable' => true,
+                        'audited' => true,
+                        'importable' => 'true',
+                        'massupdate' => false,
+                        'type' => 'varchar',
+                        'len' => 4,
+                    ),
+                ),
+                'indices' => array (),
+                'relationships' => array (),
+            ),
+        );
+        $dic = $this->create()
+            ->defaults(array (
+                'varchar' => array ('len' => 4)
+            ))
+            ->addField('my', 'varchar')
+            ->get();
+        $this->assertEquals($rd, $dic);
+    }
+
+    public function test_addActivityRelation()
+    {
+        $rd = array (
+            '_MyModule' => array (
+                'favorites' => true,
+                'fields' => array (
+                    'tasks' => array (
+                        'name' => 'tasks',
+                        'vname' => 'LBL_TASKS',
+                        'source' => 'non-db',
+                        'type' => 'link',
+                        'bean_name' => 'Task',
+                        'relationship' => '_mymodules_activities_tasks',
+                        'module' => 'Tasks',
+                    ),
+                    'notes' => array (
+                        'name' => 'notes',
+                        'vname' => 'LBL_NOTES',
+                        'source' => 'non-db',
+                        'type' => 'link',
+                        'bean_name' => 'Note',
+                        'relationship' => '_mymodules_activities_notes',
+                        'module' => 'Notes',
+                    ),
+                    'meetings' => array (
+                        'name' => 'meetings',
+                        'vname' => 'LBL_MEETINGS',
+                        'source' => 'non-db',
+                        'type' => 'link',
+                        'bean_name' => 'Meeting',
+                        'relationship' => '_mymodules_activities_meetings',
+                        'module' => 'Meetings',
+                    ),
+                    'calls' => array (
+                        'name' => 'calls',
+                        'vname' => 'LBL_CALLS',
+                        'source' => 'non-db',
+                        'type' => 'link',
+                        'bean_name' => 'Call',
+                        'relationship' => '_mymodules_activities_calls',
+                        'module' => 'Calls',
+                    ),
+                    'emails' => array (
+                        'name' => 'emails',
+                        'vname' => 'LBL_EMAILS',
+                        'source' => 'non-db',
+                        'type' => 'link',
+                        'bean_name' => 'Email',
+                        'relationship' => '_mymodules_activities_emails',
+                        'module' => 'Emails',
+                    ),
+                ),
+                'indices' => array (),
+                'relationships' => array (
+                    '_mymodules_activities_tasks' => array (
+                        'lhs_key' => 'id',
+                        'rhs_key' => 'parent_id',
+                        'relationship_type' => 'one-to-many',
+                        'relationship_role_column' => 'parent_type',
+                        'lhs_module' => '_MyModules',
+                        'lhs_table' => '_mymodules',
+                        'rhs_module' => 'Tasks',
+                        'rhs_table' => 'tasks',
+                        'relationship_role_column_value' => '_MyModules',
+                    ),
+                    '_mymodules_activities_notes' => array (
+                        'lhs_key' => 'id',
+                        'rhs_key' => 'parent_id',
+                        'relationship_type' => 'one-to-many',
+                        'relationship_role_column' => 'parent_type',
+                        'lhs_module' => '_MyModules',
+                        'lhs_table' => '_mymodules',
+                        'rhs_module' => 'Notes',
+                        'rhs_table' => 'notes',
+                        'relationship_role_column_value' => '_MyModules',
+                    ),
+                    '_mymodules_activities_meetings' => array (
+                        'lhs_key' => 'id',
+                        'rhs_key' => 'parent_id',
+                        'relationship_type' => 'one-to-many',
+                        'relationship_role_column' => 'parent_type',
+                        'lhs_module' => '_MyModules',
+                        'lhs_table' => '_mymodules',
+                        'rhs_module' => 'Meetings',
+                        'rhs_table' => 'meetings',
+                        'relationship_role_column_value' => '_MyModules',
+                    ),
+                    '_mymodules_activities_calls' => array (
+                        'lhs_key' => 'id',
+                        'rhs_key' => 'parent_id',
+                        'relationship_type' => 'one-to-many',
+                        'relationship_role_column' => 'parent_type',
+                        'lhs_module' => '_MyModules',
+                        'lhs_table' => '_mymodules',
+                        'rhs_module' => 'Calls',
+                        'rhs_table' => 'calls',
+                        'relationship_role_column_value' => '_MyModules',
+                    ),
+                    '_mymodules_activities_emails' => array (
+                        'lhs_key' => 'id',
+                        'rhs_key' => 'parent_id',
+                        'relationship_type' => 'one-to-many',
+                        'relationship_role_column' => 'parent_type',
+                        'lhs_module' => '_MyModules',
+                        'lhs_table' => '_mymodules',
+                        'rhs_module' => 'Emails',
+                        'rhs_table' => 'emails',
+                        'relationship_role_column_value' => '_MyModules',
+                    ),
+                ),
+            ),
+        );
+        $dic = $this->create()
+            ->addRelationships(array (
+                'Activites'
+            ))
+            ->get();
+        $this->assertEquals($rd, $dic);
     }
 
     public function test_Url()
@@ -539,6 +798,7 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'id_name' => 'currency_id',
                     'source' => 'non-db',
                     'type' => 'relate',
+                    'link' => 'currency_link',
                 ),
                 'currency_symbol' => array (
                     'name' => 'currency_symbol',
@@ -577,8 +837,7 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'importable' => 'true',
                     'massupdate' => false,
                     'type' => 'currency',
-                    'len' => '26,2',
-                    'dbType' => 'decimal',
+                    'dbType' => 'double',
                 ),
                 'field1_usdollar' => array (
                     'name' => 'field1_usdollar',
@@ -589,8 +848,7 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'importable' => 'true',
                     'massupdate' => false,
                     'type' => 'currency',
-                    'len' => '26,2',
-                    'dbType' => 'decimal',
+                    'dbType' => 'double',
                     'group' => 'field1',
                 ),
             ),
@@ -1104,6 +1362,7 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'table' => 'accounts',
                     'id_name' => 'account_id',
                     'module' => 'Accounts',
+                    'link' => 'account_link',
                 ),
                 'account_link' => array (
                     'name' => 'account_link',
@@ -1171,6 +1430,7 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
                     'table' => 'accounts',
                     'id_name' => 'store_id',
                     'module' => 'Accounts',
+                    'link' => 'store_link'
                 ),
                 'store_link' => array (
                     'name' => 'store_link',
@@ -1207,6 +1467,15 @@ class VardefModifier_Test extends PHPUnit_Framework_TestCase
         ));
         $d = $m->get();
         $this->assertEquals($real_dic, $d[$this->object_name]);
+    }
+
+    public function test_addContactRelationship()
+    {
+        $this->create()
+            ->addRelationships(array (
+                'Contacts'
+            ))
+            ->get();
     }
 
     public function test_addRelationshipRequired()
