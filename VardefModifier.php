e0c3e07ad6f95d28bf3ccfb87d57f24b2ba18683
Emil Kilhage <emilkilhage@gmail.com>
2016-04-12 14:23:53 +0200

    Fixed issue in currency fields

--- VardefModifier.php
+++ VardefModifier.php
@@ -1049,7 +1049,7 @@ class VardefModifier
 
             $baseSettings['calculated'] = true;
             $baseSettings['enforced'] = true;
-            $baseSettings['formula'] = "divide(\$$name,\$base_rate)";
+            $baseSettings['formula'] = "ifElse(isNumeric(\$$name), currencyDivide(\$$name, \$base_rate), \"\")";
         }
 
         return $this->
